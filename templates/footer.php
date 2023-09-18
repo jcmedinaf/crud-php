4054d9423a7aa5c84e0ddeb815866024c7f028c2
Juan Carlos Medina <jcmedinaf@gmail.com>
2023-09-17 21:26:36 -0400

    g

--- templates/footer.php
+++ templates/footer.php
@@ -8,6 +8,19 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
     </script>
+
+    <script>
+        $(document).ready(function(){
+            $('#tablaID').DataTable({
+                "pageLength": 3,
+                lenghttMenu:[
+                    
+                ],
+                "languaje"
+            });
+        });
+    </script>
+
 </body>
 
 </html>
